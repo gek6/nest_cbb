06f5d2b24a941b4449935f4f7bcde75b2a547832
gek6 <thamiti@163.com>
2021-02-20 22:49:29 +0800

    Signed-off-by: gek6 <thamiti@163.com>

--- config/weixin.config.js
+++
@@ -1,2 +0,0 @@
-exports.APPID = "wxbaaffd5522678f68"
-exports.SECRET = "65839f628c9a68d9eb8257e1f88ae7e7"
--- src/service/user/user.service.ts
+++ src/service/user/user.service.ts
@@ -3,55 +3,53 @@ import { InjectModel } from '@nestjs/mongoose';
 import axios from "axios";
 import * as wxConfig from "../../../config/weixin.config.js";
 import { JwtAuthService } from "../jwt-auth/jwt-auth.service"
-@Injectable()  
+@Injectable()
 export class UserService {
     constructor(
         private jwtAuthService: JwtAuthService,
-        @InjectModel('User') private __model){
+        @InjectModel('User') private __model) {
 
     }
     // 通过微信服务器登录
-    async wxmpDoLogin(code){
+    async wxmpDoLogin(code) {
         const APPID = wxConfig.APPID;
-        const SECRET = wxConfig.SECRET; 
+        const SECRET = wxConfig.SECRET;
         let wxUrl = `https://api.weixin.qq.com/sns/jscode2session?appid=${APPID}&secret=${SECRET}&js_code=${code}&grant_type=authorization_code`
 
-          
+
         let httpRes = await axios.get(wxUrl);
         // console.log(httpRes)
         return httpRes.data;
     }
 
     // 数据库查询
-    async find(queryJson = {}){
+    async find(queryJson = {}) {
         return await this.__model.find(queryJson);
     }
 
-    async findByOpenid(openid)
-    {
+    async findByOpenid(openid) {
         return await this.__model.findOne({
             openid
         })
     }
 
-    async add(rowJson){
+    async add(rowJson) {
         console.log('add');
-        
+
         let User = new this.__model({
             ...rowJson,
         });
         let saveRes = await User.save();
         console.log(saveRes);
         let token = this.jwtAuthService.encodeToken({
-            openid:saveRes.openid
+            openid: saveRes.openid
         })
         return token
-        
+
     }
 
 
-    async createToken(openid)
-    {
+    async createToken(openid) {
         let token = this.jwtAuthService.encodeToken({
             openid
         })
